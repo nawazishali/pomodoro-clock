480d3d50024d85f9170d0233f64ac34ec9d42b3b
Nawazish Ali <nawazishaliejaz@gmail.com>
2017-03-17 19:47:31 +0500

    Basic clock is now working

--- main.js
+++ main.js
@@ -13,12 +13,67 @@ function decrement(targetId) {
 function increment(targetId) {
     var targetId = document.getElementById(targetId);
     var currentVal = parseInt(targetId.innerHTML);
-    var newVal = (currentVal + 1).toString();
-    targetId.innerHTML = newVal;
+    if (currentVal >= 60) {
+        return false;
+    } else {
+        var newVal = (currentVal + 1).toString();
+        targetId.innerHTML = newVal;
+    }
+}
+
+function formatTime(time) {
+    var seconds = time % 60;
+    var mins = (time - seconds) / 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    return mins + ":" + seconds;
+}
+
+
+var clock = document.getElementById('clock');
+var sessionInSeconds = parseInt(document.getElementById('session-time').innerHTML) * 60;
+var breakinSeconds = parseInt(document.getElementById('break-time').innerHTML) * 60;
+var sessionT = sessionInSeconds;
+var breakT = breakinSeconds;
+
+function startTimer() {
+    var sessionTimer = setInterval(function () {
+        clock.innerHTML = formatTime(sessionT);
+        sessionT--;
+        if (sessionT < 0) {
+            clearInterval(sessionTimer);
+            var breakTimer = setInterval(function () {
+                clock.innerHTML = formatTime(breakT);
+                breakT--;
+                if (breakT < 0) {
+                    clearInterval(breakTimer);
+                    sessionT = sessionInSeconds;
+                    breakT = breakinSeconds;
+                    startTimer();
+                }
+            }, 1000);
+        }
+    }, 1000);
 
 }
 
-function displayTimer() {
-    var clock = document.getElementById('clock');
-
+function stopTimer() {
+    for (var i = 1; i < 10; i++) {
+        window.clearInterval(i);
+    }
 }
+
+var toggleBtn = document.getElementById('timer-toggle');
+toggleBtn.onclick = function () {
+    if (toggleBtn.innerHTML === "Stop Timer") {
+        toggleBtn.innerHTML = "Start Timer";
+        stopTimer();
+    } else {
+        toggleBtn.innerHTML = "Stop Timer";
+        startTimer();
+    }
+};
